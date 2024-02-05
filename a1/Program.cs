2948ac6447b612ddbbc504abc569b8f7f0692515
manis <manis@MANISH>
2024-02-05 17:26:25 -0500

    Add project files.

---
+++ a1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("\nPlease choose a type of pet:");
+Console.WriteLine("1. Dog");
+Console.WriteLine("2. Cat");
+Console.WriteLine("3. Rabbit");
+Console.Write("Enter your choice: ");
+string petType = Console.ReadLine();
+
+Console.Write("Enter your pet's name: ");
+string petName = Console.ReadLine();
